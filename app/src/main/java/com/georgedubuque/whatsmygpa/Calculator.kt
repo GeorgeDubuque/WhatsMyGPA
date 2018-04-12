f91578f9d7ed7a5ae9fc2a0921db006948ea95e9
GeorgeDubuque <gtdubuque@gmail.com>
2018-04-11 22:44:58 -0700

    Implemented toGradePoint function which converts grade percentage to grade point, and add onClick to GPA button. Calculate GPA functionality is now working with a static 4 classes. Made some UI changes

--- app/src/main/java/com/georgedubuque/whatsmygpa/Calculator.kt
+++ app/src/main/java/com/georgedubuque/whatsmygpa/Calculator.kt
@@ -5,8 +5,14 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_calculator.*
+import kotlinx.android.synthetic.main.content_calculator.*
+import java.text.DecimalFormat
 
 class Calculator : AppCompatActivity() {
 
@@ -14,6 +20,10 @@ class Calculator : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
         setSupportActionBar(toolbar)
+
+//        GpaButton.setOnClickListener { Toast.makeText (
+//                this@Calculator, "GPA Button Pressed", Toast.LENGTH_SHORT).show()}
+        GpaButton.setOnClickListener {generateGPA()}
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -31,4 +41,57 @@ class Calculator : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    fun generateGPA(){
+        var gp1 = toGradePoint(perc1.text.toString().toDouble())
+        var gp2 = toGradePoint(perc2.text.toString().toDouble())
+        var gp3 = toGradePoint(perc3.text.toString().toDouble())
+        var gp4 = toGradePoint(perc4.text.toString().toDouble())
+
+        var cw1 = cw1.text.toString().toInt()
+        var cw2 = cw2.text.toString().toInt()
+        var cw3 = cw3.text.toString().toInt()
+        var cw4 = cw4.text.toString().toInt()
+
+        var totalGradePoints = gp1*cw1 + gp2*cw2 + gp3*cw3 + gp4*cw4
+        var totalCreditHours = cw1 + cw2 + cw3 + cw4
+
+        Toast.makeText(this@Calculator, (totalGradePoints/totalCreditHours).toString(), Toast.LENGTH_SHORT).show()
+    }
+
+    fun toGradePoint(decimalGrade: Double):Double{
+        var gradePoint: Double = 0.00;
+        if(decimalGrade >= 93){
+            gradePoint = 4.0
+        }else if(decimalGrade >= 90 && decimalGrade < 93){
+            gradePoint = 3.70
+        }else if(decimalGrade >= 87 && decimalGrade < 90){
+            gradePoint = 3.33
+        }else if(decimalGrade >= 83 && decimalGrade < 87) {
+            gradePoint = 3.0
+        }else if(decimalGrade >= 80 && decimalGrade < 83) {
+            gradePoint = 2.70
+        }else if(decimalGrade >= 77 && decimalGrade < 80) {
+            gradePoint = 2.30
+        }else if(decimalGrade >= 73 && decimalGrade < 77) {
+            gradePoint = 2.00
+        }else if(decimalGrade >= 70 && decimalGrade < 73) {
+            gradePoint = 1.70
+        }else if(decimalGrade >= 67 && decimalGrade < 70) {
+            gradePoint = 1.30
+        }else if(decimalGrade >= 63 && decimalGrade < 67) {
+            gradePoint = 1.00
+        }else if(decimalGrade >= 60 && decimalGrade < 63) {
+            gradePoint = 0.70
+        }else if(decimalGrade >= 0 && decimalGrade < 60) {
+            gradePoint = 0.00
+        }
+
+        return gradePoint
+    }
+
+    fun addGrade(){
+        Toast.makeText(this@Calculator, "Need to add grade.", Toast.LENGTH_LONG).show()
+    }
+
 }
